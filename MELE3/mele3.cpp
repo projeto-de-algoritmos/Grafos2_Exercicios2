e494f33f5fac463bdda1f9160aa70a2e204b84e0
arthurmlv <arthurmv123@gmail.com>
2023-10-15 14:40:09 -0300

    style: melhora legibilidade e adiciona comentários

--- MELE3/mele3.cpp
+++ MELE3/mele3.cpp
@@ -24,19 +24,21 @@ void dijkstra(int f, int l, Item lista[]){
         pos = pq.top().second;
         c = pq.top().first;
         pq.pop();
-        d = distancia[lista[pos].dado];
+        d = distancia[lista[pos].dado]; // distancia do nó atual
         if(l==pos){
             printf("%d\n", d*5);
             return;
         }
         vis[lista[pos].dado] = 1;
-        for(int i = 0; i<lista[pos].count; ++i){
-            if(vis[lista[pos].prox[i].fim]==1) continue;
+        for(int i = 0; i<lista[pos].count; ++i){  // percorre lista de adjacencia
+            int index = lista[pos].prox[i].fim; // armazena o dado do fim do elevador
+            if(vis[index]==1) continue; // vai pro prox se tiver visitado
             falta = 0;
-            v = abs(lista[pos].dado-lista[pos].prox[i].fim);
+            v = abs(lista[pos].dado-index);
             if(d!=0){
-                if(lista[pos].prox[i].offset==1){ // elevador reverso, inicia atrasado na outra ponta (offset+1)
-                    ajuste = (d/v+1)%2;
+                int offset = lista[pos].prox[i].offset;
+                if(offset){ // elevador reverso, inicia atrasado na outra ponta (offset=1, meio ciclo de ida e volta a mais)
+                    ajuste = (d/v+offset)%2;
                     if(ajuste==1){
                         if(d%v==0) falta = v;
                         else falta = v-(d%v);
@@ -45,7 +47,7 @@ void dijkstra(int f, int l, Item lista[]){
                         else falta = (v-d%v)+v;
                     }
                 }
-                else {                           // elevador normal, inicia com os outros
+                else {     // elevador normal (offset=0), inicia com os outros elevadores normais
                     ajuste = (d/v)%2;
                     if(ajuste==0){
                         if(d%v==0) falta = 0;
@@ -56,10 +58,10 @@ void dijkstra(int f, int l, Item lista[]){
                     }
                 }
             }
-            if((v+c+falta)<distancia[lista[pos].prox[i].fim]){
-                distancia[lista[pos].prox[i].fim] = falta+v+c; // ?
-                kx = lista[pos].prox[i].fim;
-                pq.push(make_pair(distancia[lista[pos].prox[i].fim], kx));
+            if((v+c+falta)<distancia[index]){
+                distancia[index] = falta+v+c; // ?
+                kx = index;
+                pq.push(make_pair(distancia[index], kx));
             }   
         }
     }
@@ -77,12 +79,10 @@ int main(){
         cin >> b;
         elevators[a].dado = a;
         elevators[a].prox[elevators[a].count].fim = b;
-        elevators[a].prox[elevators[a].count].offset = 0;
-        elevators[a].count++;
+        elevators[a].prox[elevators[a].count++].offset = 0;
         elevators[b].dado = b;
         elevators[b].prox[elevators[b].count].fim = a;
-        elevators[b].prox[elevators[b].count].offset = 1;
-        elevators[b].count++;
+        elevators[b].prox[elevators[b].count++].offset = 1;
     }
     dijkstra(1, k, elevators);
     return 0;
